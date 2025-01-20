8fd5e846ea82f150e472fc9ee78cd3f0d0acbe57
Eugênio Dias <eugeniodias5@hotmail.com>
2025-01-20 18:40:26 +0100

    Adding code to the repository

---
+++ data/__init__.py
@@ -0,0 +1,2 @@
+from data import offline_sampler, online_sampler
+from data import yt_dataset
---
+++ models/Bifor.py
@@ -0,0 +1,27 @@
+import torch
+from torchvision.models import convnext_small, convnext_large
+import torch.nn as nn
+
+class Bifor(nn.Module):
+    def __init__(self):
+        super(Bifor, self).__init__()
+        # Load the pre-trained ConvNeXT model
+        model = convnext_large(weights="IMAGENET1K_V1")
+
+        # Remove the classifier
+        self.feature_extractor = nn.Sequential(*(list(model.children())[:-1]))
+        
+    def forward(self, x):
+        # Forward pass through the feature extractor
+        features = self.feature_extractor(x)
+        features = features.squeeze(-1).squeeze(-1)
+        
+        return features
+    
+    def infer(self, x):
+        # Checking if the image is in a batch
+        if len(x.shape) == 3:
+            x = x.unsqueeze(0) # Convert the image to a batch format
+
+        return self.forward(x)
+    
